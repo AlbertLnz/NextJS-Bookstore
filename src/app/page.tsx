3e683c196558e2ab366ea19a47545fb1379b8e21
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 20:27:10 +0100

    Simulating a delay of 3s like an API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ import { Book } from './types';
 
 export default async function IndexPage(){
 
-  const books: Book[] = await import('../books.json').then((data) => data.library.map((obj) => obj.book))
+  const books: Book[] = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('../books.json').then((data) => data.library.map((obj) => obj.book)))
+    }, 3000)
+  })
   const genres: Book['genre'][] = Array.from(new Set(books.map(book => book.genre)))
 
   return <IndexClientPage books={books} genres={genres} />
