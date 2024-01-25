28e7a43d62caa129fb55a8a16171e8b5c9bedf97
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 17:31:18 +0100

    books console log + Book & Author interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+'use client' // Client component -> (states, interactions...)
+
+import data from '../books.json'
+import { Book, Author } from './types';
+
+// console.log(data)
+
+
+const books: Book[] = data.library.map((d) => d.book)
+console.log(books )
+
 export default function Home() {
   return (
     <main>
