bff900914e745bfbcc2222de4b7e28b27fed0627
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 17:43:07 +0100

    genre filter functionability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client' // Client component -> (states, interactions...)
 
+import { useState } from 'react'
 import data from '../books.json'
 import { Book, Author } from './types';
 
@@ -10,10 +11,17 @@ const books: Book[] = data.library.map((d) => d.book)
 console.log(books )
 
 export default function Home() {
+
+  const [genre, setGenre] = useState<string>("");
+  const matches = genre ? books.filter(book => {
+    if(genre && book.genre !== genre) return false
+    return true
+  }) : books
+
   return (
     <article className='grid gap-4'>
       <nav>
-        <select>
+        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
           <option value="">Todos</option>
           <option value="Ciencia ficción">Ciencia ficción</option>
           <option value="Fantasía">Fantasía</option>
@@ -22,7 +30,7 @@ export default function Home() {
         </select>
       </nav>
       <ul className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
-        {books.map((book) => (
+        {matches.map((book) => (
           <li key={book.ISBN}>
             <img src={book.cover} className='aspect-[9/14] object-cover' alt={book.title} />
             <p>{book.title}</p>
