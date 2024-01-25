c2334458b729d8bf96e88a651698a259e1552509
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 20:36:21 +0100

    export Promise out of IndexPage function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@
 import IndexClientPage from './client';
 import { Book } from './types';
 
+const api = {
+  book: {
+    list: async (): Promise<Book[]> => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import('../books.json').then((data) => data.library.map((obj) => obj.book)))
+        }, 3000)
+      })
+    }
+  }
+}
+
 export default async function IndexPage(){
 
-  const books: Book[] = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(import('../books.json').then((data) => data.library.map((obj) => obj.book)))
-    }, 3000)
-  })
+  const books = await api.book.list()
   const genres: Book['genre'][] = Array.from(new Set(books.map(book => book.genre)))
 
   return <IndexClientPage books={books} genres={genres} />
