5200803a3bc26e31cd9bdae38f881a3db0747b0a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 17:38:17 +0100

    Selector of genre w/o functionability yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,24 @@ console.log(books )
 
 export default function Home() {
   return (
-    <ul className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
-      {books.map((book) => (
-        <li key={book.ISBN}>
-          <img src={book.cover} className='aspect-[9/14] object-cover' alt={book.title} />
-          <p>{book.title}</p>
-        </li>
-      ))}
-    </ul>
+    <article className='grid gap-4'>
+      <nav>
+        <select>
+          <option value="">Todos</option>
+          <option value="Ciencia ficción">Ciencia ficción</option>
+          <option value="Fantasía">Fantasía</option>
+          <option value="Terror">Terror</option>
+          <option value="Zombies">Zombies</option>
+        </select>
+      </nav>
+      <ul className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
+        {books.map((book) => (
+          <li key={book.ISBN}>
+            <img src={book.cover} className='aspect-[9/14] object-cover' alt={book.title} />
+            <p>{book.title}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
   );
 }
