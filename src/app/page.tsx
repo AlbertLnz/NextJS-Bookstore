c8479a856184e170eca8b82c5bbbdf2df04d1a95
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 20:25:16 +0100

    Import books.json like a API (through Promise)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 // Server component
 
-import data from '../books.json'
 import IndexClientPage from './client';
 import { Book } from './types';
 
+export default async function IndexPage(){
 
-const books: Book[] = data.library.map((d) => d.book)
-const genres: Book['genre'][] = Array.from(new Set(books.map(book => book.genre)))
+  const books: Book[] = await import('../books.json').then((data) => data.library.map((obj) => obj.book))
+  const genres: Book['genre'][] = Array.from(new Set(books.map(book => book.genre)))
 
-
-export default function IndexPage(){
   return <IndexClientPage books={books} genres={genres} />
 }
