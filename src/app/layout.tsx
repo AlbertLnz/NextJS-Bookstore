59d1d30633d294512ab87cc4e23a00b8f2bb832d
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-25 17:19:54 +0100

    Some styles to Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <main className="grid grid-rows-[60px,1fr,60px] p-4 m-auto gap-4 max-w-screen-lg min-h-screen">
+          <nav className="flex items-center text-2xl">NavBar</nav>
+          <section>{children}</section>
+          <footer className="flex items-center justify-center">Footer</footer>
+        </main>
+      </body>
     </html>
   );
 }
